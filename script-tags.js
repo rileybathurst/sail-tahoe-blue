354ff7de906b85c407921373bb1813161a0dcee7
Riley Bathurst <riley@rileybathurst.com>
2026-09-01 12:29:20 -0700

    offer catalog

---
+++ script-tags.js
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+  {
+  "@context": "https://schema.org",
+  "@type": "TouristTrip",
+  "name": "Sail Tahoe Blue",
+  "description": "Private sailing charters and public boat tours in South Lake Tahoe offering sunset cruises, swim charters, and custom sailing experiences.",
+  "image": "https://static.wixstatic.com/media/d3ac5e_b5a85c5df177437e98a477d3fd004b7c~mv2.jpg/v1/fill/w_1908,h_960,al_c,q_85,enc_avif,quality_auto/d3ac5e_b5a85c5df177437e98a477d3fd004b7c~mv2.jpg",
+  "url": "https://sailtahoeblue.com",
+  "telephone": "5302133451",
+  "areaServed": {
+    "@type": "City",
+    "name": "South Lake Tahoe"
+  },
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "South Lake Tahoe",
+    "addressRegion": "CA",
+    "addressCountry": "US"
+  },
+  "sameAs": [
+    "https://www.instagram.com/sailtahoeblue",
+    "https://www.facebook.com/sailtahoeblue"
+  ]
+}
+</script>
